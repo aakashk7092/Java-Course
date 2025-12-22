b94ebfb958c621af1d7fb4219905506d2451ae23
Aakash Kumar <your-email@gmail.com>
2025-12-22 17:16:22 +0530

    More about class

--- Demo.java
+++ Demo.java
@@ -1,15 +1,23 @@
-class Calculator{
-    int a ;
-    public int add(){
-        System.out.println("in add");
-        return 0;
+class Computer
+{
+    public void  playMusic()
+    {
+        System.out.println("Music Playing ......");
+    }
+    public String getMePen(int cost)
+    {
+        if(cost >=5){
+            return "Pen";
+        }else{
+            return "Add some more Money 5 or more than 5 rupees";
+        }
+        
     }
 }
 public class Demo {
     public static void main(String[] args) {
-        int num1 = 4;
-        int num2 = 4;
-        Calculator calc = new Calculator() ;
-        calc.add();
+        Computer obj = new Computer();
+        obj.playMusic();
+        System.out.println(obj.getMePen(1));
     }
 }
