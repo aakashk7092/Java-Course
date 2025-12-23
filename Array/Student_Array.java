3b6e497f67bc5916c67413492bef9b4997367cdb
Aakash Kumar <your-email@gmail.com>
2025-12-24 00:26:19 +0530

    Implementation of Student Array

---
+++ Array/Student_Array.java
@@ -0,0 +1,34 @@
+class  Student{
+    int roll_no;
+    String name;
+    int marks;
+}
+public class Student_Array {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.roll_no = 1;
+        s1.name = "Aakash";
+        s1.marks = 99;
+        Student s2 = new Student();
+        s2.roll_no = 2;
+        s2.name = "Yogi";
+        s2.marks = 99;
+        Student s3 = new Student();
+        s3.roll_no = 1;
+        s3.name = "NITIN";
+        s3.marks = 99;
+
+        Student students[] = new Student[3];
+
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+
+        for(int i = 0 ; i < students.length;i++){
+            System.out.println( students[i].roll_no +"  :  "+students[i].name +"  :  "+ students[i].marks);
+        }
+
+        
+
+    }
+}
